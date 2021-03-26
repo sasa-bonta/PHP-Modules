d5428ac0ba4fcf83df16407e62e3408dfcb93122
sasa-bonta <bonta2000@mail.ru>
2021-03-26 18:12:04 +0200

    Search criteria OK. Place for exceptions are marked

--- Module/ProductCatalogServiceInterface.php
+++ Module/ProductCatalogServiceInterface.php
@@ -6,9 +6,9 @@ interface ProductCatalogServiceInterface {
 
     # CRUD
     public function getProductByCode(string $productCode): Product;    # @todo ProductNotFoundException
-    public function searchProduct(SearchCriteria $sc): ProductCollection;
+    public function searchProduct(SearchCriteria $sc): ProductCollection; # @todo =: Exceptions in //Search Criteria
     public function createProduct(Product $product): bool;  # @todo ProductCodeDuplicateException
-    public function updateProduct(Product $product): bool;
-    public function deleteProductByCode(string $productCode): bool;  # @todo SearchCriteriaInvalidPageException and  SearchCriteriaInvalidLimitException
+    public function updateProduct(Product $product): bool;   # @todo ProductNotFoundException
+    public function deleteProductByCode(string $productCode): bool;
 
 }
